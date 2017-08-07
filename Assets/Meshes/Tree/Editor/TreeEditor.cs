3e621e42604c09eb7e1e2b99bb3d24213274735d
Peros <pierre_w@live.com>
2017-08-07 18:30:15 +0200

    Controller Added, Tree Added

---
+++ Assets/Meshes/Tree/Editor/TreeEditor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Castle))]
+public class TreeEditor : Editor
+{
+    Tree tree;
+
+    void Awake()
+    {
+		tree = target as Tree;
+    }
+
+	public override void OnInspectorGUI()
+	{
+		EditorGUI.BeginChangeCheck();
+		#region Parameters
+		tree.size = EditorGUILayout.Slider("Size", tree.size, 1f, 100f);
+		tree.diameter = EditorGUILayout.Slider("Diameter", tree.diameter, 1f, 100f);
+		#endregion // Parameters
+
+		EditorGUILayout.BeginVertical("box");
+		GUILayout.Label("Maintexture");
+		tree.mainTexture =
+			EditorGUILayout.ObjectField(
+				"MainTexture",
+				tree.mainTexture,
+				typeof(Texture2D),
+				true) as Texture2D;
+
+		EditorGUILayout.EndVertical();
+
+		if (EditorGUI.EndChangeCheck())
+			tree.CreateMesh();
+	}
+}
