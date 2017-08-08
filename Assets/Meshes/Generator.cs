132b81f83626e4b9ba0362fc97fa448a841c9df8
Peros Ölkmann <pierre_w@gmx.de>
2017-08-08 19:19:51 +0200

    Added Skybox, Camera-Movement and Generator

---
+++ Assets/Meshes/Generator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour {
+
+    List<GameObject> GroundTiles = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Reset()
+    {
+        GenerateGround(Vector3.zero);
+    }
+
+    void GenerateGround(Vector3 position, int Width = 1, int Height = 1, int Depth = 1, float groundHeight = 0.8f)
+    {
+        GameObject gameObject = new GameObject();
+
+        gameObject.AddComponent<Ground>();
+        gameObject.GetComponent<Ground>().Width = Width;
+        gameObject.GetComponent<Ground>().Height = Height;
+        gameObject.GetComponent<Ground>().Depth = Depth;
+        gameObject.GetComponent<Ground>().groundHeight = groundHeight;
+
+        GroundTiles.Add(gameObject);
+    }
+}
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,23 +7,23 @@ public class PlayerController : MonoBehaviour {
 
 	private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
 
-	void Update()
-	{
-		if(Input.GetKey(KeyCode.RightArrow))
-		{
-			transform.Translate(new Vector3(speed * Time.deltaTime,0,0));
-		}
-		if(Input.GetKey(KeyCode.LeftArrow))
-		{
-			transform.Translate(new Vector3(-speed * Time.deltaTime,0,0));
-		}
-		if(Input.GetKey(KeyCode.DownArrow))
-		{
-			transform.Translate(new Vector3(0,-speed * Time.deltaTime,0));
-		}
-		if(Input.GetKey(KeyCode.UpArrow))
-		{
-			transform.Translate(new Vector3(0,speed * Time.deltaTime,0));
-		}
-	}
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(new Vector3(0, -speed * Time.deltaTime, 0));
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
+        }
+    }
 }
