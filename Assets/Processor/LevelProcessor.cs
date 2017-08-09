b5d63250d7fe33fd046e7f6cb7dba48815240e20
Peros Ölkmann <pierre_w@gmx.de>
2017-08-09 17:05:43 +0200

    LevelProcessor

--- Assets/Processor/LevelProcessor.cs
+++ Assets/Processor/LevelProcessor.cs
@@ -16,6 +16,9 @@ public class LevelProcessor : MonoBehaviour
     public float steepProbability = 0.5f;
     public float blockProbability = 0.3f;
 
+    private List<GameObject> leftWorld = new List<GameObject>();
+    private List<GameObject> rightWorld = new List<GameObject>();
+
     public void Reset()
     {
         ProcessLevel();
@@ -99,6 +102,91 @@ public class LevelProcessor : MonoBehaviour
 
     void DrawMap(Level[,] map)
     {
+        DestroyOldWorld();
+        ChangeRightToLeftWorld();
 
+
+        // build new rightWorld 
+        BuildRightGround(map);
+    }
+
+    void DestroyOldWorld()
+    {
+        for (int i = 0; i < leftWorld.Count; i++)
+        {
+            Destroy(leftWorld[i]);
+        }
+    }
+
+    void ChangeRightToLeftWorld()
+    {
+        leftWorld = rightWorld;
+    }
+
+    void BuildRightGround(Level[,] map)
+    {
+        int GroundHeight = 0, LastGroundHeight = 0;
+        int GroundWidth = 0;
+        int StartPosition = 0;
+
+
+        for (int x = 0; x < map.GetLength(1); x++)
+        {
+            Debug.Log("X = " + (x + 1));
+
+            if (map[map.GetLength(0) - 1, x] == Level.GROUND)
+            {
+                if (GroundWidth == 0)
+                    StartPosition = x;
+
+                GroundWidth++;
+
+                for (int y = map.GetLength(0) - 1; y > 0; y--)
+                {
+                    if (map[y, x] == Level.GROUND)
+                        GroundHeight++;
+
+                    Debug.Log("Y = " + (y + 1));
+                }
+
+                if (GroundHeight > LastGroundHeight)
+                {
+                    // We need to build the Ground-Object
+                    // and start with the next one
+
+                    Debug.Log(string.Format("Position = {0}; GroundWeidth = {1}; GroundHeight = {2}", StartPosition, GroundWidth, GroundHeight));
+                    LastGroundHeight = GroundHeight;
+                    CreateGround(StartPosition, GroundWidth, GroundHeight);
+                    GroundHeight = 0;
+                    GroundWidth = 0;
+                    continue;
+                }
+
+                //Debug.Log(string.Format("Ende X: Position = {0}; GroundWeidth = {1}; GroundHeight = {2}", StartPosition, GroundWidth, GroundHeight));
+                LastGroundHeight = GroundHeight;
+                GroundHeight = 0;
+            }
+
+
+        }
+    }
+
+
+    private void CreateGround(int StartPosition, int GroundWidth, int GroundHeight)
+    {
+        GameObject ground = new GameObject();
+
+        ground.AddComponent<Ground>();
+        Debug.Log(string.Format("Ground: StartPosition = {0}; GroundWidth = {1}; GroundHeight = {2}", StartPosition, GroundWidth, GroundHeight));
+        ground.GetComponent<Ground>().Height = GroundHeight;
+        ground.GetComponent<Ground>().Width = GroundWidth;
+        ground.GetComponent<Ground>().Depth = 1;
+        ground.GetComponent<Ground>().CreateMesh();
+
+        Transform transform = ground.GetComponent<Transform>();
+        transform.position = new Vector2(StartPosition, 0);
+
+        ground.transform.parent = this.transform;
+        leftWorld.Add(ground);
     }
 }
