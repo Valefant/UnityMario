0aa137ec4ce2bcb2c30b32a1d25ce29c16691f36
Valentino <valinho@web.de>
2017-08-09 09:02:45 +0200

    Create skeleton for level generator

---
+++ Assets/Generators/LevelGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public int columns = 100;
+    public int rows = 25;
+    public int playerHeight = 1;
+    public int maxJumpHeight = 1;
+
+    void GenerateLevel()
+    {
+        int[,] map = new int[rows, columns];
+
+
+    }
+
+    void GenerateGround(int[,] map)
+    {
+
+    }
+
+    void GenerateBlocks(int[,] map)
+    {
+
+    }
+
+    void GenerateObstacles(int[,] map)
+    {
+
+    }
+}
