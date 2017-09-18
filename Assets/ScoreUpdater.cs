cdb4f9b0c5a748a8ac5af380014307756893ddb5
Valefant <valinho@web.de>
2017-09-18 21:18:37 +0200

    Point counter works now

--- Assets/ScoreUpdater.cs
+++ Assets/ScoreUpdater.cs
@@ -7,25 +7,13 @@ using UnityEngine.UI;
 public class ScoreUpdater : MonoBehaviour {
 
     Text txt;
-    TheEventSystem.EventHandler eventHandler;
-    // Use this for initialization
+    
     void Start()
     {
-        TheEventSystem tES = TheEventSystem.getInstance();
         txt = gameObject.GetComponent<Text>();
-
-        eventHandler = (e) => txt.text = ((PickupEvent)e).getPoints().ToString();
         txt.text = "Score: ";
-        tES.addEventHandler(typeof(PickupEvent).GetType().Name, (e) => txt.text = "Score :" + ((PickupEvent)e).getPoints());
-        
-
-        
-        //txt.text = "Score : ";
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+		TheEventSystem.getInstance().addEventHandler("PickupEvent", (e) => {
+			txt.text = "Score :" + ((PickupEvent)e).getPoints();
+		});
     }
 }
