2ed5485fe92b9f89e962cb27b11a2b773c31eda4
Pierre Wilken <pierre_w@live.com>
2017-09-19 20:00:40 +0200

    menu audio

--- Assets/Game Jam Template/Scripts/SetAudioLevels.cs
+++ Assets/Game Jam Template/Scripts/SetAudioLevels.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class SetAudioLevels : MonoBehaviour {
 
-	public AudioMixer mainMixer;					//Used to hold a reference to the AudioMixer mainMixer
+	public AudioMixer mainMixer; //Used to hold a reference to the AudioMixer mainMixer
 
 
 	//Call this function and pass in the float parameter musicLvl to set the volume of the AudioMixerGroup Music in mainMixer
