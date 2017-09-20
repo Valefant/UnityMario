82f02c51dcca89bcd2d83f6a442c5da30dc72057
Valefant <valinho@web.de>
2017-09-20 21:41:23 +0200

    Add leftover files

---
+++ Assets/DisplaySeed.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.EventSystem;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplaySeed : MonoBehaviour
+{
+    private Text _text;
+    
+    void Start()
+    {
+        _text = gameObject.GetComponent<Text>();
+        _text.text = "Seed: " + LevelProcessor.Seed;
+    }
+}
